694122954dadaba1b3dc86096764f2d9d0792ffa
danielclauser <clauser.daniel@gmail.com>
2025-01-26 17:59:08 +0100

    Refactored part6

--- python/tennis6.py
+++ python/tennis6.py
@@ -1,72 +1,67 @@
 class TennisGame6:
-    def __init__(self, player1_name, player2_name):
+    player1_name: str
+    player2_name: str
+    player1_score: int
+    player2_score: int
+
+    def __init__(self, player1_name: str, player2_name: str) -> None:
         self.player1_name = player1_name
         self.player2_name = player2_name
         self.player1_score = 0
         self.player2_score = 0
 
-    def won_point(self, player_name):
+    def won_point(self, player_name: str) -> None:
         if player_name == "player1":
             self.player1_score += 1
         else:
             self.player2_score += 1
 
+    def _get_score_name(self, points: int, have_same_score: bool) -> str:
+        score_name: str
+        match points:
+            case 0:
+                score_name = "Love"
+            case 1:
+                score_name = "Fifteen"
+            case 2:
+                score_name = "Thirty"
+            case _:
+                if have_same_score:
+                    score_name = "Deuce"
+                else:
+                    score_name = "Forty"
+        if score_name != "Deuce" and have_same_score:
+            score_name += "-All"
+        return score_name
+
     def score(self):
         result: str
-
-        if self.player1_score == self.player2_score:
-            # tie score
-            tie_score: str
-            match self.player1_score:
-                case 0:
-                    tie_score = "Love-All"
-                case 1:
-                    tie_score = "Fifteen-All"
-                case 2:
-                    tie_score = "Thirty-All"
-                case _:
-                    tie_score = "Deuce"
-
-            result = tie_score
+        have_same_score = self.player1_score == self.player2_score
+        if have_same_score:
+            result = self._get_score_name(self.player1_score, have_same_score)
         elif self.player1_score >= 4 or self.player2_score >= 4:
             # end-game score
             end_game_score: str
-
-            if self.player1_score - self.player2_score == 1:
-                end_game_score = "Advantage " + self.player1_name
-            elif self.player1_score - self.player2_score == -1:
-                end_game_score = "Advantage " + self.player2_name
-            elif self.player1_score - self.player2_score >= 2:
-                end_game_score = "Win for " + self.player1_name
-            else:
-                end_game_score = "Win for " + self.player2_name
+            diff_score = self.player1_score - self.player2_score
+            match diff_score:
+                case 1:
+                    end_game_score = "Advantage " + self.player1_name
+                case -1:
+                    end_game_score = "Advantage " + self.player2_name
+                case diff_score if diff_score >= 2:
+                    end_game_score = "Win for " + self.player1_name
+                case _:
+                    end_game_score = "Win for " + self.player2_name
 
             result = end_game_score
         else:
-            # regular score
-            regular_score: str
-
-            match self.player1_score:
-                case 0:
-                    score1 = "Love"
-                case 1:
-                    score1 = "Fifteen"
-                case 2:
-                    score1 = "Thirty"
-                case _:
-                    score1 = "Forty"
-
-            match self.player2_score:
-                case 0:
-                    score2 = "Love"
-                case 1:
-                    score2 = "Fifteen"
-                case 2:
-                    score2 = "Thirty"
-                case _:
-                    score2 = "Forty"
-
-            regular_score = score1 + "-" + score2
+            player1_score_name = self._get_score_name(
+                self.player1_score, have_same_score
+            )
+            player2_score_name = self._get_score_name(
+                self.player2_score, have_same_score
+            )
+            regular_score = player1_score_name + "-" + player2_score_name
 
             result = regular_score
 
