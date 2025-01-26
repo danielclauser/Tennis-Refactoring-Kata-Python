a6097e7d93a703d2e9586f17d90453b21f32ad57
danielclauser <clauser.daniel@gmail.com>
2025-01-26 20:55:16 +0100

    Part5

--- python/tennis5.py
+++ python/tennis5.py
@@ -1,56 +1,64 @@
 class TennisGame5:
-    def __init__(self, player1_name, player2_name):
+    player1_name: str
+    player2_name: str
+    player1_score: int
+    player2_score: int
+    POINTS_NAME: dict = {
+        0: "Love",
+        1: "Fifteen",
+        2: "Thirty",
+        3: "Forty",
+    }
+
+    def __init__(self, player1_name: str, player2_name: str) -> None:
         self.player1_name = player1_name
         self.player2_name = player2_name
         self.player1_score = 0
         self.player2_score = 0
 
-    def won_point(self, player_name):
-        if player_name == "player1":
+    def won_point(self, player_name: str) -> None | ValueError:
+        if player_name == self.player1_name:
             self.player1_score += 1
-        elif player_name == "player2":
+        elif player_name == self.player2_name:
             self.player2_score += 1
         else:
             raise ValueError("Invalid player name.")
 
-    def score(self):
-        p1 = self.player1_score
-        p2 = self.player2_score
-
-        while p1 > 4 or p2 > 4:
-            p1 -= 1
-            p2 -= 1
-
-        lookup = {
-            (0, 0): "Love-All",
-            (0, 1): "Love-Fifteen",
-            (0, 2): "Love-Thirty",
-            (0, 3): "Love-Forty",
-            (0, 4): "Win for player2",
-            (1, 0): "Fifteen-Love",
-            (1, 1): "Fifteen-All",
-            (1, 2): "Fifteen-Thirty",
-            (1, 3): "Fifteen-Forty",
-            (1, 4): "Win for player2",
-            (2, 0): "Thirty-Love",
-            (2, 1): "Thirty-Fifteen",
-            (2, 2): "Thirty-All",
-            (2, 3): "Thirty-Forty",
-            (2, 4): "Win for player2",
-            (3, 0): "Forty-Love",
-            (3, 1): "Forty-Fifteen",
-            (3, 2): "Forty-Thirty",
-            (3, 3): "Deuce",
-            (3, 4): "Advantage player2",
-            (4, 0): "Win for player1",
-            (4, 1): "Win for player1",
-            (4, 2): "Win for player1",
-            (4, 3): "Advantage player1",
-            (4, 4): "Deuce",
-        }
-
-        entry = (p1, p2)
-        if entry in lookup:
-            return lookup[entry]
+    def _get_score_name(self, points: int) -> str:
+        if points < 4:
+            return self.POINTS_NAME[points]
         else:
-            raise ValueError("Invalid score.")
+            return ""
+
+    def _get_leader_name(self) -> str:
+        if self.player1_score > self.player2_score:
+            return self.player1_name
+        else:
+            return self.player2_name
+
+    def score(self) -> str | ValueError:
+        player1_score: str = self.player1_score
+        player2_score: str = self.player2_score
+        result: str = ""
+        while player1_score > 4 or player2_score > 4:
+            player1_score -= 1
+            player2_score -= 1
+
+        player1_score_name = self._get_score_name(player1_score)
+        player2_score_name = self._get_score_name(player2_score)
+        leader_name = self._get_leader_name()
+        delta_score = abs(player1_score - player2_score)
+        min_score = min(player1_score, player2_score)
+        if delta_score == 0:
+            if player1_score <= 2:
+                result = player1_score_name + "-All"
+            else:
+                result = "Deuce"
+        elif delta_score == 4 - min_score:
+            if min_score < 3:
+                result = f"Win for {leader_name}"
+            else:
+                result = f"Advantage {leader_name}"
+        else:
+            result = player1_score_name + "-" + player2_score_name
+        return result
