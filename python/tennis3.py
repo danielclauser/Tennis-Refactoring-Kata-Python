31dba04b712c6ce0c774b8d89fc770ee9da8b5dc
danielclauser <clauser.daniel@gmail.com>
2025-01-26 17:30:08 +0100

    Refactored part3

--- python/tennis3.py
+++ python/tennis3.py
@@ -1,27 +1,43 @@
 class TennisGame3:
-    def __init__(self, player1_name, player2_name):
-        self.p1_n = player1_name
-        self.p2_n = player2_name
-        self.p1 = 0
-        self.p2 = 0
+    player1_name: str
+    player2_name: str
+    player1_points: int
+    player2_points: int
+    POINTS_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
 
-    def won_point(self, n):
+    def __init__(self, player1_name: str, player2_name: str) -> None:
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.player1_points = 0
+        self.player2_points = 0
+
+    def won_point(self, n: str) -> None:
         if n == "player1":
-            self.p1 += 1
+            self.player1_points += 1
         else:
-            self.p2 += 1
+            self.player2_points += 1
 
-    def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+    def score(self) -> str:
+        if (self.player1_points < 4 and self.player2_points < 4) and (
+            self.player1_points + self.player2_points < 6
+        ):
+            player1_score = self.POINTS_NAMES[self.player1_points]
+            player2_score = self.POINTS_NAMES[self.player2_points]
+            if self.player1_points == self.player2_points:
+                return player1_score + "-All"
+            else:
+                return player1_score + "-" + player2_score
         else:
-            if self.p1 == self.p2:
+            if self.player1_points == self.player2_points:
                 return "Deuce"
-            s = self.p1_n if self.p1 > self.p2 else self.p2_n
-            return (
-                "Advantage " + s
-                if ((self.p1 - self.p2) * (self.p1 - self.p2) == 1)
-                else "Win for " + s
+            leader = (
+                self.player1_name
+                if self.player1_points > self.player2_points
+                else self.player2_name
             )
+            if (self.player1_points - self.player2_points) * (
+                self.player1_points - self.player2_points
+            ) == 1:
+                return "Advantage " + leader
+            else:
+                return "Win for " + leader
